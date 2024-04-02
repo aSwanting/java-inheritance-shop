663890f468522df6b9b7478f5127783c1d770d15
aSwanting <gab.damico@gmail.com>
2024-04-02 14:52:14 +0200

    add - setters (name, description, price)

--- src/org/learning/javainheritanceshop/Product.java
+++ src/org/learning/javainheritanceshop/Product.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class Product {
     private int code;
     private String name, description;
-    private BigDecimal price, priceWithVAT;
+    private BigDecimal price;
     private final BigDecimal VAT = BigDecimal.valueOf(0.20);
 
     public Product(String name, String description, BigDecimal price) {
@@ -25,6 +25,10 @@ public class Product {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getExtendedName() {
         return String.format("%08d", code) + "-" + name.toLowerCase().replace(" ", "");
     }
@@ -33,13 +37,19 @@ public class Product {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public BigDecimal getPrice() {
         return price.setScale(2, RoundingMode.HALF_EVEN);
     }
 
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
     public BigDecimal getPriceWithVAT() {
-        return (
-                price.add(price.multiply(VAT))
-        ).setScale(2, RoundingMode.HALF_EVEN);
+        return (price.add(price.multiply(VAT))).setScale(2, RoundingMode.HALF_EVEN);
     }
 }
